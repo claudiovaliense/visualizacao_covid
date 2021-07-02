b16cfe1f99b5f1e51f4218ec0c3d9567a140ca55
Claudio Valiense <claudiovaliense@gmail.com>
2021-07-02 02:31:22 -0300

    adicionado varios graficos em um unico local

--- baseline.py
+++ baseline.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt # plot static
 import plotly.express as px # plot dynamic
 import datetime # time
 import statistics # function statistics
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly
 
 def read_data():
     """Read data"""
@@ -21,37 +24,56 @@ def read_data():
             vacinas_estado[ dados[index][3] ] = {'qtd' : 0, 'age' : [] }
         vacinas_estado[ dados[index][3] ] ['qtd'] +=1  # estado    
         vacinas_estado[ dados[index][3] ] ['age'].append(age)  # idade    
+    
     vacinas_estado = cv.remove_key_dict(vacinas_estado, 'XX'); vacinas_estado = cv.remove_key_dict(vacinas_estado, 'paciente_endereco_uf')    
-        
     for k in vacinas_estado:
-        vacinas_estado[k]['age'] = statistics.mean( vacinas_estado[k]['age'] )
+        vacinas_estado[k]['age'] = statistics.mean( vacinas_estado[k]['age'] )   
         
-    
-    #''' # vacina idade
-    fig = px.bar(x=vacinas_estado.keys(), y=[vacinas_estado[k]['age'] for k in vacinas_estado])    
-    fig.update_layout(xaxis_title='Estados', yaxis_title='Média da idade dos vacinados')
-    fig.show()    
-    fig.write_html("html/vacina_idade_estado.html")
-    #'''
-    
-    ''' # vacina estado
-    fig = px.bar(x=vacinas_estado.keys(), y=vacinas_estado.values())    
+    fig = px.bar(x=list(vacinas_estado.keys()), y=[vacinas_estado[k]['qtd'] for k in vacinas_estado]) 
     fig.update_layout(xaxis_title='Estados', yaxis_title='Quantidade de vacinas')
-    fig.show()    
+    #fig.show()    
     fig.write_html("html/vacina_estado.html")
-    '''
-    
-    print( datetime.datetime.today() )
-    
-
 
 
 if __name__ == "__main__":
     read_data()
 
-# backup
-''' # estatico
+# backup -----------------
+
+''' # gráfico estatico
 plt.bar(vacinas_estado.keys(), vacinas_estado.values())
 plt.xlabel('Estados')
 plt.ylabel('Quantidade de vacinas')
 plt.show()'''
+
+'''  # uso de dash  
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from base64 import b64encode
+import io
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig),
+    dcc.Graph(figure=fig2)
+])    
+app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter
+'''
+
+''' # salvar figura em html separado
+fig2 = px.bar(x=list(vacinas_estado.keys()), y=[vacinas_estado[k]['qtd'] for k in vacinas_estado]) 
+fig2.update_layout(xaxis_title='Estados', yaxis_title='Quantidade de vacinas')
+fig2.show()    
+fig2.write_html("html/vacina_estado.html")
+'''
+
+''' # exemplo com subplot
+figsub = make_subplots(rows=2, cols=1)        
+figsub.add_trace( go.Bar(x=list(vacinas_estado.keys()), y=[vacinas_estado[k]['qtd'] for k in vacinas_estado]), 1, 1) 
+figsub.add_trace( go.Bar(x=list(vacinas_estado.keys()), y=[vacinas_estado[k]['age'] for k in vacinas_estado]), 2, 1) 
+figsub.update_xaxes(title_text="Estados", row=1, col=1); figsub.update_yaxes(title_text="Quantidade de vacinas", row=1, col=1)
+figsub.update_xaxes(title_text="Estados", row=2, col=1); figsub.update_yaxes(title_text="Média da idade dos vacinados", row=2, col=1)        
+figsub.update_layout(showlegend=False) 
+figsub.show() 
+figsub.write_html("html/vacina.html")
+'''
