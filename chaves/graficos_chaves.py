cfbd30a1a22c294905523f4934f5d6eed6195249
Frederico Carvalho <37462139+fccarvalho2@users.noreply.github.com>
2021-08-15 12:05:03 -0300

    Update graficos_chaves.py
    Incluídos botões radiais para seleção do tipo de gráfico a ser mostrado. 
    Simplificação dos callbacks para evitar chamadas  redundantes à mesma função.
    Callbacks agora recebem dois inputs: o dropdown e o valor do botão radial selecionado.

--- chaves/graficos_chaves.py
+++ chaves/graficos_chaves.py
@@ -7,7 +7,38 @@ import pandas as pd
 import os
 
 class GraficoChaves:
-
+    def atualizar(self, estado, tipo):
+        dir_path = os.path.dirname(os.path.realpath(__file__)) + "/"
+        df = pd.read_csv(dir_path + "casos.csv")
+        if estado == 'Todos':
+            e = 'br'
+        else:
+            e = estado.lower()
+            
+        df = df.loc[df["Estado"] == e]
+        if tipo == "O":
+            x_M =-df.Obitos_M
+            x_F = df.Obitos_F
+        else:
+            x_M =-df.Casos_M
+            x_F = df.Casos_F
+            
+        fig = [go.Bar(y = pd.unique(df.Idade),
+                       x=x_F, 
+                       name = "Mulheres",
+                       orientation ='h',
+                       hoverinfo='x',
+                       marker=dict(color="lightpink")
+                      ),
+                go.Bar(y = pd.unique(df.Idade),
+                       x=x_M, 
+                       name = "Homens",
+                       orientation ='h',
+                       hoverinfo='x',
+                       marker=dict(color="dodgerblue")
+                      )]
+        return {'data':fig}
+    
     def comparativo_casos(self, app):
         dir_path = os.path.dirname(os.path.realpath(__file__)) + "/"
         df = pd.read_csv(dir_path + "casos.csv")
@@ -16,7 +47,8 @@ class GraficoChaves:
         
         component_html = html.Div([
             html.Hr(),
-            html.H4('Pirâmide etária de casos confirmados até 11/08/2021'),
+            html.H3('Casos e óbitos confirmados até 11/08/2021'),
+            html.H5('Nesta visualização são mostradas duas pirâmides etárias, o que facilita comparação entre estados ou casos e óbitos de um mesmo estado. Fonte: OpenDataSUS'),
             html.Div([
                 html.Div([
                     html.Div([html.Label('Selecione o estado'),
@@ -27,9 +59,16 @@ class GraficoChaves:
                     value = 'Todos',
                     )
                 ], style={'width': '50%'}),
+                    html.Div([dcc.RadioItems(
+                    id = 'tipo1',
+                    options=[
+                    {'label': "Casos", 'value': 'C'},
+                    {'label': 'Óbitos', 'value': 'O'},
+                    ], value='C',
+                    labelStyle={'display': 'inline-block'})
+                    ]),
                     dcc.Graph(id = 'piramide-casos1')
                 ], style={'width': '47%', 'display': 'inline-block'}),
-                
                 html.Div([
                     html.Div([html.Label('Selecione o estado'),
                     dcc.Dropdown(
@@ -39,71 +78,33 @@ class GraficoChaves:
                     value = 'Todos',
                     )
                 ], style={'width': '50%'}),
-                    dcc.Graph(id = 'piramide-casos2')            
+                html.Div([dcc.RadioItems(
+                    id = 'tipo2',
+                    options=[
+                    {'label': "Casos", 'value': 'C'},
+                    {'label': 'Óbitos', 'value': 'O'},
+                    ], value='C',
+                    labelStyle={'display': 'inline-block'})
+                    ]),
+                    dcc.Graph(id = 'piramide-casos2')   
                 ], style={'width': '47%', 'display': 'inline-block', 'align': 'right'})
             ])
         ])
         
         @app.callback(
-            Output('piramide-casos1', 'figure'),
-            Input('drop_estados1', 'value')
+        Output('piramide-casos1', 'figure'),
+        Input('drop_estados1', 'value'),
+        Input('tipo1', 'value')
         )
-        
-        def update_piramide1(estado):
-            dir_path = os.path.dirname(os.path.realpath(__file__)) + "/"
-            df = pd.read_csv(dir_path + "casos.csv")
-            if estado == 'Todos':
-                e = 'br'
-            else:
-                e = estado.lower()
-            
-            df = df.loc[df["Estado"] == e]
-            
-            fig = [go.Bar(y = pd.unique(df.Idade),
-                           x=df.Casos_F, 
-                           name = "Mulheres",
-                           orientation ='h',
-                           hoverinfo='x',
-                           marker=dict(color="lightpink")
-                          ),
-                    go.Bar(y = pd.unique(df.Idade),
-                           x=-df.Casos_M, 
-                           name = "Homens",
-                           orientation ='h',
-                           hoverinfo='x',
-                           marker=dict(color="dodgerblue")
-                          )]
-            return {'data':fig}
+        def update_piramide1(estado, tipo):
+            return self.atualizar(estado, tipo)
         
         @app.callback(
             Output('piramide-casos2', 'figure'),
-            Input('drop_estados2', 'value')
+            Input('drop_estados2', 'value'),
+            Input('tipo2', 'value')
         )
-        
-        def update_piramide2(estado):
-            dir_path = os.path.dirname(os.path.realpath(__file__)) + "/"
-            df = pd.read_csv(dir_path + "casos.csv")
-            if estado == 'Todos':
-                e = 'br'
-            else:
-                e = estado.lower()
-            
-            df = df.loc[df["Estado"] == e]
-            
-            fig = [go.Bar(y = pd.unique(df.Idade),
-                           x=df.Casos_F, 
-                           name = "Mulheres",
-                           orientation ='h',
-                           hoverinfo='x',
-                           marker=dict(color="lightpink")
-                          ),
-                    go.Bar(y = pd.unique(df.Idade),
-                           x=-df.Casos_M, 
-                           name = "Homens",
-                           orientation ='h',
-                           hoverinfo='x',
-                           marker=dict(color="dodgerblue")
-                          )]
-            return {'data':fig}
+        def update_piramide2(estado, tipo):
+            return self.atualizar(estado, tipo)
         
         return [component_html]
